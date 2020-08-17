10b176ced8db319199a167a71188944345227732
김서영 <43196103+sy406@users.noreply.github.com>
2020-08-18 00:43:37 +0900

    Create Q6_1.c

---
+++ week6/Q6_1.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stack{
+    int top; //count
+    int capacity;
+    int* array; //data
+} Stack;
+
+Stack* createStack(int capacity) {
+    Stack* stack = (Stack*)malloc(sizeof(Stack));
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->array = (int *)malloc(stack->capacity*sizeof(int));
+    return stack;
+}
+
+int isFull(Stack* stack) {
+    return stack->top == stack->capacity-1;
+}
+
+int isEmpty(Stack* stack) {
+    return stack->top == -1;
+}
+
+void push(Stack* stack, int item) {
+    if (isFull(stack))
+        return;
+    stack->array[++stack->top] = item;
+    printf("%d pushed to stack\n", item);
+}
+
+int pop(Stack* stack) {
+    if(isEmpty(stack)){
+        printf("-9999 pop from stack\n");
+        exit(-1);
+    }
+    return stack->array[stack->top--];
+}
+
+int peek(Stack* stack) {
+    if(isEmpty(stack)){
+        printf("-9999 pop from stack\n");
+        exit(-1);
+    }else {
+        return stack->array[stack->top];
+    }
+}
+
+int main() {
+    Stack* stack = createStack(100);
+
+    push(stack, 10);
+    push(stack, 20);
+    push(stack, 30);
+    push(stack, 40);
+
+    printf("%d pop from stack\n", pop(stack));
+    printf("%d pop from stack\n", pop(stack));
+
+    push(stack, 50);
+    printf("%d pop from stack\n", pop(stack));
+    printf("%d pop from stack\n", pop(stack));
+    printf("%d pop from stack\n", pop(stack));
+    printf("%d pop from stack\n", pop(stack));
+    return 0;
+}
