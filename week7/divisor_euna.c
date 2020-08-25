6a86216433af4824a797b5b03d8e02ffe13a750b
aeuna <4294psy@naver.com>
2020-08-25 23:58:07 +0900

    week7 homework

---
+++ week7/divisor_euna.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *divisor;
+
+int
+compare(const void * a, const void * b)
+{
+    return (*(int*)a - *(int*)b);
+}
+
+int
+main()
+{
+    int N;
+    scanf("%d", &N);
+    
+    divisor = malloc(sizeof(int) * N);
+
+    for(int i = 0; i < N; i++){
+        int tmp;
+        scanf("%d", &tmp);
+        divisor[i] = tmp;
+    }
+
+    qsort(divisor, N, sizeof(int), compare);
+
+    int answer = divisor[0]*divisor[N-1];
+    printf("%d\n", answer);
+
+    return 0;
+}
